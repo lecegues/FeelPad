5b89910413bd8a5d229b33d2a41bb579b369482f
programmer-b <ProgB@cs.usask.ca>
2023-10-23 12:52:51 -0600

    Added all the required Images for the image buttons to drawable. Implemented the add note button which takes user to new note page upon clicking. Removed app:layout_allignParentBottom="true" to fix error Added back code from the original MainActivity.java that I previously deleted in my implementation.

--- journalapp/app/src/main/java/com/example/journalapp/MainActivity.java
+++ journalapp/app/src/main/java/com/example/journalapp/MainActivity.java
@@ -1,62 +1,96 @@
 package com.example.journalapp;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.ImageButton;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.lifecycle.ViewModelProvider;
+        import androidx.recyclerview.widget.LinearLayoutManager;
+        import androidx.recyclerview.widget.RecyclerView;
+
+        import com.example.journalapp.note.NoteViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private NoteListAdapter noteListAdapter;
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainpage_activity);
-        ImageButton arrowButton = findViewById(R.id.arrowdown);
-        ImageButton combinePdfButton = findViewById(R.id.combinePDF);
-        ImageButton addNoteButton = findViewById(R.id.addNote);
-        ImageButton searchButton = findViewById(R.id.search);
-        ImageButton templateButton = findViewById(R.id.template);
+        setContentView(R.layout.activity_main);
+        setNoteRecyclerView();
+        createNoteObserver();
 
-        arrowButton.setOnClickListener(new View.OnClickListener() {
+
+         ImageButton arrowButton = findViewById(R.id.arrowdown);
+         ImageButton combinePdfButton = findViewById(R.id.combinePDF);
+         ImageButton addNoteButton = findViewById(R.id.addNote);
+         ImageButton searchButton = findViewById(R.id.search);
+         ImageButton templateButton = findViewById(R.id.template);
+
+         arrowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Handle the click for the arrow button here
             }
-        });
-
-        combinePdfButton.setOnClickListener(new View.OnClickListener() {
+       });
+         combinePdfButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Handle the click for the combine PDF button here
             }
-        });
-
-        addNoteButton.setOnClickListener(new View.OnClickListener() {
+         });
+         addNoteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Handle the click for the add note button here
                 Intent intent = new Intent(MainActivity.this, NewNoteActivity.class);
                 startActivity(intent);
             }
-        });
-
-        searchButton.setOnClickListener(new View.OnClickListener() {
+         });
+         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Handle the click for the search button here
             }
-        });
+         });
 
-        templateButton.setOnClickListener(new View.OnClickListener() {
+         templateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Handle the click for the template button here
             }
+         });
+    }
+
+
+    private void createNoteObserver() {
+        NoteViewModel noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, notes -> {
+            noteListAdapter.submitList(notes);
         });
     }
 
+    private void setNoteRecyclerView() {
+        RecyclerView noteRecycleView = findViewById(R.id.noteListView);
+        noteListAdapter = new NoteListAdapter(new NoteListAdapter.NoteDiff());
+        noteRecycleView.setAdapter(noteListAdapter);
+        noteRecycleView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public void newNote(View view) {
+        Intent intent = new Intent(this, NewNoteActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setNoteRecyclerView();
+        setNoteRecyclerView();
+    }
 
 }
