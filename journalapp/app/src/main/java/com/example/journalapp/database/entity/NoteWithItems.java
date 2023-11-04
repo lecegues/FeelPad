1b38f20b5ad094c0a5d899ce9142d7a4f6759cc3
dbj592 <john.lecegues@usask.ca>
2023-11-04 15:44:55 -0600

    Added javadocs and comments to new Data-Layer Classes & Functions

--- journalapp/app/src/main/java/com/example/journalapp/database/entity/NoteWithItems.java
+++ journalapp/app/src/main/java/com/example/journalapp/database/entity/NoteWithItems.java
@@ -10,15 +10,16 @@ import java.util.List;
 
 /**
  * Links two Entity databases together: Note & NoteItemEntity
+ * Represents a one-to-many relationship between a Note entity and a NoteItemEntity
  */
 public class NoteWithItems {
-    @Embedded
+    @Embedded // indicates that fields of the Note class are to be treated as if they were part of this class
     public Note note;
 
     @Relation(
             parentColumn = "id",
             entityColumn = "note_id"
     )
-    public List<NoteItemEntity> items;
+    public List<NoteItemEntity> items; // List of note items associated with the note
 
 }
--- journalapp/app/src/main/java/com/example/journalapp/utils/ItemTypeConverter.java
+++ journalapp/app/src/main/java/com/example/journalapp/utils/ItemTypeConverter.java
@@ -5,15 +5,28 @@ import androidx.room.TypeConverter;
 import com.example.journalapp.ui.note.NoteItem;
 
 /**
- * Type Converters
+ * Room Type Converters
+ * (Room does not support complex data types, so we can convert them to types that SQLite understands)
  */
 public class ItemTypeConverter {
 
-    @TypeConverter
+    /**
+     * Converts an integer back to the corresponding NoteItem.ItemType enum
+     * Used when reading data from the database
+     * @param itemType
+     * @return
+     */
+    @TypeConverter // tells room that they should be used for converting between the database column type and entity field type
     public static NoteItem.ItemType toItemType(int itemType){
         return NoteItem.ItemType.values()[itemType];
     }
 
+    /**
+     * Converts a NoteItem.ItemType to its ordinal integer value.
+     * Used when writing data to database
+     * @param itemType
+     * @return
+     */
     @TypeConverter
     public static int toInteger(NoteItem.ItemType itemType){
         return itemType.ordinal();
