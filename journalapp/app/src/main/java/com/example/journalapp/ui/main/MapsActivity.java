41a8af3135ba3e01618497fce8cc75e45e2cab78
kanwarsingh <kanwar16112309@gmail.com>
2023-11-16 13:10:39 -0600

    fixing activity_maps.xml

--- journalapp/app/src/main/java/com/example/journalapp/ui/main/MapsActivity.java
+++ journalapp/app/src/main/java/com/example/journalapp/ui/main/MapsActivity.java
@@ -18,6 +18,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.journalapp.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
