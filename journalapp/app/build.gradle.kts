ed5e8b085b6c9f6675751658013c95e628433b89
kanwarsingh <kanwar16112309@gmail.com>
2023-11-16 01:32:32 -0600

    added dependencies in manifest file

--- journalapp/app/build.gradle.kts
+++ journalapp/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.journalapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.journalapp"
@@ -52,4 +52,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
 }
