7cc3ea0983e8a83dcbe48121b74827e1de78539b
dbj592 <john.lecegues@usask.ca>
2023-11-19 22:08:16 -0600

    Created a FTS Table (NoteFtsEntity.java) + methods and updated Database to include table
    - FTS Table will be used to store text contents of a note together for easier search queries

---
+++ journalapp/app/src/main/java/com/example/journalapp/database/entity/NoteFtsEntity.java
@@ -0,0 +1,66 @@
+package com.example.journalapp.database.entity;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Fts4;
+
+/**
+ * Full-Text-Search (FTS) Table to allow easier searching/filtering queries
+ */
+@Entity
+@Fts4
+public class NoteFtsEntity {
+
+    @ColumnInfo(name = "rowid")
+    private int rowid; // required by room to be used as primary key in FTS tables. Must be named rowid
+
+    @ColumnInfo(name = "noteId")
+    private String noteId; // used to link FTS entry back to original Note
+
+    @ColumnInfo(name = "combinedText")
+    private String combinedText; // Contains all text content added together from TEXT enum types for a given note
+
+    /**
+     * Constructor for NoteFtsEntity
+     * @param noteId
+     * @param combinedText
+     */
+    public NoteFtsEntity(String noteId, String combinedText){
+        this.noteId = noteId;
+        this.combinedText = combinedText;
+    }
+
+    public int getRowid() {
+        return rowid;
+    }
+
+    public void setRowid(int rowid) {
+        this.rowid = rowid;
+    }
+
+    public String getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(String noteId) {
+        this.noteId = noteId;
+    }
+
+    public String getCombinedText() {
+        return combinedText;
+    }
+
+    public void setCombinedText(String combinedText) {
+        this.combinedText = combinedText;
+    }
+
+    /*
+    Use to strip all HTML tags to be in NoteFtsEntity
+    public static String stripHtmlTags(String html) {
+    if (html == null) {
+        return "";
+    }
+    return html.replaceAll("<[^>]*>", "").trim();
+}
+     */
+}
