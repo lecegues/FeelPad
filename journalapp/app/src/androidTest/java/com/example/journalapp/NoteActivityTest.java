0b313e304450b15b8aa6a782150fedc7c433cdfd
Nathan Tsandelis <n.tsandman@gmail.com>
2023-11-19 22:06:20 -0600

    Compiles I guess

--- journalapp/app/src/androidTest/java/com/example/journalapp/NoteActivityTest.java
+++ journalapp/app/src/androidTest/java/com/example/journalapp/NoteActivityTest.java
@@ -3,29 +3,40 @@ package com.example.journalapp;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import android.content.Context;
 import android.net.Uri;
 
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.journalapp.ui.note.NoteActivity;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runner.manipulation.Ordering;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteActivityTest {
 
-    private NoteActivity noteActivity;
-
-    public NoteActivityTest(NoteActivity noteActivity) {
-        this.noteActivity = noteActivity;
-    }
+    private static final boolean IS_IMAGE = true;
 
     @Test
-    public void saveMediaToInternalStorage_Test(){
-        Uri randomUri = Uri.parse("content://mock-uri");
-        Uri newUri = noteActivity.saveMediaToInternalStorage(randomUri, true);
-        assertNotNull(newUri);
+    public void saveMediaToInternalStorage_Test() {
+
+        try (ActivityScenario<NoteActivity> scenario = ActivityScenario.launch(NoteActivity.class)) {
+            scenario.onActivity(activity -> {
+                ApplicationProvider.getApplicationContext();
+
+                Uri randomUri = Uri.parse("content://mock-uri");
+
+                NoteActivity noteActivity = new NoteActivity();
+
+                Uri newUri = noteActivity.saveMediaToInternalStorage(randomUri, IS_IMAGE);
+
+                assertNull(newUri);
+            });
+        }
     }
 
 
