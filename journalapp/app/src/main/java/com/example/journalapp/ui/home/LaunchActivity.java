a0565b95c09f67093c6377b8e2685f0ee7d65219
dbj592 <john.lecegues@usask.ca>
2023-12-01 05:39:18 -0600

    Added javadocs and relevant comments to `Home` folder

--- journalapp/app/src/main/java/com/example/journalapp/ui/home/LaunchActivity.java
+++ journalapp/app/src/main/java/com/example/journalapp/ui/home/LaunchActivity.java
@@ -12,6 +12,10 @@ import com.example.journalapp.ui.main.BottomNavBarFragment;
 import com.example.journalapp.ui.main.TopNavBarFragment;
 import com.google.android.material.button.MaterialButton;
 
+/**
+ * Initial activity shown when User's first time opening the app; doesn't open after first time
+ * Handles first-time setup and theme application
+ */
 public class LaunchActivity extends AppCompatActivity implements NameDialogFragment.NameDialogListener {
 
     private MaterialButton btnWrite;
@@ -44,25 +48,36 @@ public class LaunchActivity extends AppCompatActivity implements NameDialogFragm
                 .replace(R.id.bottomNavBarFragmentContainer, new BottomNavBarFragment())
                 .commit();
 
+        // initialize buttons
         btnWrite = findViewById(R.id.launch_activity_btn);
-
         btnWrite.setOnClickListener(v ->{
             showNameDialog();
 
         });
     }
 
+    /**
+     * Checks if it's the first time the app is being launched
+     * @return true if it's the first time, otherwise, false
+     */
     private boolean isFirstTime() {
         SharedPreferences preferences = getSharedPreferences("AppPreferences", MODE_PRIVATE);
         return preferences.getBoolean("isFirstTime", true);
     }
 
+    /**
+     * Shows the name dialog for first-time users.
+     */
     private void showNameDialog() {
         NameDialogFragment dialog = new NameDialogFragment();
         dialog.show(getSupportFragmentManager(), "NameDialogFragment");
 
     }
 
+    /**
+     * Sets the first-time launch flag using SharedPreference
+     * @param isFirstTime boolean flag indicating if it's the user's first time opening the app
+     */
     private void setFirstTimeFlag(boolean isFirstTime) {
         SharedPreferences preferences = getSharedPreferences("AppPreferences", MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
@@ -70,24 +85,36 @@ public class LaunchActivity extends AppCompatActivity implements NameDialogFragm
         editor.apply();
     }
 
+    /**
+     * Callback from the NameDialogFragment
+     * When name is entered in the fragment, will jump to this function
+     * @param name String name
+     */
     @Override
     public void onNameEntered(String name) {
-        // Save the name in SharedPreferences or handle it as needed
+
+        // save name to SharedPreferences
         SharedPreferences preferences = getSharedPreferences("AppPreferences", MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString("PreferredName", name);
         editor.apply();
 
-        // set the firstTimeflag after user enters their name
+        // first-time-flag is set once user gives their name
         if (isFirstTime()) {
             setFirstTimeFlag(false);
         }
 
-        // after this, start Activity
+        // start activity
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Retrieves the theme ID based on the provided theme name.
+     * Exists in every activity when applying the assigned theme
+     * @param themeName String themeName (from SharedPreferences)
+     * @return an integer representing the theme
+     */
     private int getThemeId(String themeName) {
         switch (themeName) {
             case "Blushing Tomato":
